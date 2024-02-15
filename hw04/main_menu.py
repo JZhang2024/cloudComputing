39ba81bba439f4e00e540f0eb513a429f0b0b29c
JZhang2024 <jackie.zhang@uconn.edu>
2024-02-15 18:54:32 -0500

    implemented upload/backup for large files > 5GB

--- hw04/main_menu.py
+++ hw04/main_menu.py
@@ -1,8 +1,10 @@
 '''hw04/main_menu.py'''
 
 import os
+import time
 import boto3
 from botocore.exceptions import NoCredentialsError
+from botocore.exceptions import BotoCoreError, ClientError
 
 s3 = boto3.resource('s3')
 
@@ -24,7 +26,7 @@ def select_bucket():
 
 def backup_files(bucket_name, folder_path):
     '''Backup files from a folder to a bucket in S3.'''
-    s3 = boto3.client('s3')
+    s3_client = boto3.client('s3')
     if not os.path.exists(folder_path):
         print("Folder does not exist.")
         return
@@ -33,14 +35,17 @@ def backup_files(bucket_name, folder_path):
         print("Bucket does not exist.")
         return
     #splits large files into smaller chunks for faster upload
-    transfer_config = boto3.s3.transfer.TransferConfig(multipart_threshold=1024**3, multipart_chunksize=1024**3)
-    
+    transfer_config = boto3.s3.transfer.TransferConfig(multipart_threshold=1024**3,
+                                                       multipart_chunksize=1024**3)
     for subdir, _, files in os.walk(folder_path):
         for file in files:
             full_path = os.path.join(subdir, file)
             with open(full_path, 'rb') as data:
                 try:
-                    s3.upload_fileobj(data, bucket_name, full_path[len(folder_path)+1:], Config=transfer_config)
+                    s3_client.upload_fileobj(data,
+                                      bucket_name,
+                                      full_path[len(folder_path)+1:],
+                                      Config=transfer_config)
                     print(f"File {full_path} backed up successfully.")
                 except NoCredentialsError:
                     print("Credentials not available")
@@ -75,7 +80,7 @@ def list_object_version_info(bucket_name, object_name):
         versions = s3.Bucket(bucket_name).object_versions.filter(Prefix=object_name)
         return versions
     except NoCredentialsError:
-        print("Credentials not available")
+        return "Credentials not available"
 
 def delete_object(bucket_name, object_name):
     '''Delete an object from a bucket.'''
@@ -85,23 +90,52 @@ def delete_object(bucket_name, object_name):
     except NoCredentialsError:
         print("Credentials not available")
 
-def upload(bucket_name, localFolderName):
-    '''Uploads a folder to an S3 bucket. Files are uploaded one at a time and folder structure is preserved.'''
-    s3 = boto3.client('s3')
-    for subdir, _, files in os.walk(localFolderName):
+
+
+def upload(bucket_name, local_folder_name):
+    '''Uploads a folder to an S3 bucket.
+    Files are uploaded one at a time and folder structure is preserved.'''
+    s3_client = boto3.client('s3')
+    #splits large files into smaller chunks for faster upload
+    transfer_config = boto3.s3.transfer.TransferConfig(multipart_threshold=1024**3,
+                                                       multipart_chunksize=1024**3)
+    for subdir, _, files in os.walk(local_folder_name):
         for file in files:
             full_path = os.path.join(subdir, file)
-            with open(full_path, 'rb') as data:
-                s3.upload_fileobj(data, bucket_name, full_path[len(localFolderName):],
-                                  Config=boto3.s3.transfer.TransferConfig(multipart_threshold=1024**3)) #automatically handles large files
+            key = full_path[len(local_folder_name):]
+            s3_obj = s3.Object(bucket_name, key)
+            if s3_obj.key not in list_objects(bucket_name):
+                pass
+            elif s3_obj.last_modified.timestamp() > os.path.getmtime(full_path):
+                continue  # Skip this file because it has not changed
+            for _ in range(3):  # Retry up to 3 times
+                try:
+                    with open(full_path, 'rb') as data:
+                        s3_client.upload_fileobj(data,
+                                          bucket_name,
+                                          key,
+                                          Config=transfer_config)
+                    break  # The upload was successful, so we break out of the retry loop
+                except BotoCoreError as upload_exception:
+                    print(f"Error uploading {full_path} to {bucket_name}: {upload_exception}")
+                    time.sleep(5)  # Wait for 5 seconds before retrying
+                except IOError as upload_exception:
+                    print(f"Error reading file {full_path}: {upload_exception}")
+                    break  # There's no point in retrying if we can't read the file
 
-def list_contents(bucket_name, serverFolderName):
+def list_contents(bucket_name, server_folder_name):
     '''List the contents of a folder in an S3 bucket.'''
-    return [obj.key for obj in s3.Bucket(bucket_name).objects.filter(Prefix=serverFolderName)]
+    return [obj.key for obj in s3.Bucket(bucket_name).objects.filter(Prefix=server_folder_name)]
 
-def get_file(bucket_name, serverFolderName, file_name):
+def get_file(bucket_name, server_folder_name, file_name):
     '''Download a file from a folder in an S3 bucket.'''
-    s3.Bucket(bucket_name).download_file(serverFolderName + '/' + file_name, file_name)
+    try:
+        s3.Object(bucket_name, server_folder_name + '/' + file_name).load()
+        s3.Bucket(bucket_name).download_file(server_folder_name + '/' + file_name, file_name)
+        print("File downloaded successfully")
+    except ClientError as not_found_exception:
+        print("Either the specified folder/file does not exist or another error occurred: ",
+              not_found_exception)
 
 def main_menu():
     '''Main menu for the S3 application.'''
@@ -144,6 +178,9 @@ def main_menu():
             bucket_name = select_bucket()
             object_name = input("Enter the name of the object: ")
             versions = list_object_version_info(bucket_name, object_name)
+            if versions == "Credentials not available":
+                print("Credentials not available")
+                continue
             for version in versions:
                 print(version.get())
         elif choice == 7:
@@ -152,19 +189,19 @@ def main_menu():
             delete_object(bucket_name, object_name)
         elif choice == 8:
             bucket_name = select_bucket()
-            localFolderName = input("Enter the name of the folder: ")
-            upload(bucket_name, localFolderName)
+            local_folder_name = input("Enter the name of the folder: ")
+            upload(bucket_name, local_folder_name)
         elif choice == 9:
             bucket_name = select_bucket()
-            serverFolderName = input("Enter the name of the folder: ")
-            contents = list_contents(bucket_name, serverFolderName)
+            server_folder_name = input("Enter the name of the folder: ")
+            contents = list_contents(bucket_name, server_folder_name)
             for content in contents:
                 print(content)
         elif choice == 10:
             bucket_name = select_bucket()
-            serverFolderName = input("Enter the name of the folder: ")
+            server_folder_name = input("Enter the name of the folder: ")
             file_name = input("Enter the name of the file: ")
-            get_file(bucket_name, serverFolderName, file_name)
+            get_file(bucket_name, server_folder_name, file_name)
         elif choice == 11:
             break
         else:
--- hw04/main_menu_test.py
+++ hw04/main_menu_test.py
@@ -1,12 +1,28 @@
+'''test_main_menu.py - This file contains the tests for the main_menu.py file.
+The tests are written using the pytest framework and the moto library to mock AWS services'''
+
 import shutil
+import os
 from moto import mock_aws
 import boto3
-import os
-from main_menu import list_buckets, select_bucket, backup_files, list_objects, download_object, generate_presigned_url, delete_object, upload, list_contents, get_file
 import pytest
+from main_menu import (
+    list_buckets,
+    select_bucket,
+    backup_files,
+    list_objects,
+    download_object,
+    generate_presigned_url,
+    delete_object,
+    upload,
+    list_contents,
+    get_file
+)
 
 @mock_aws
 def test_list_buckets():
+    '''Test the list_buckets function.
+    This function should return a list of all the buckets in the account.'''
     conn = boto3.resource('s3', region_name='us-east-1')
     conn.create_bucket(Bucket='bucket1')
     conn.create_bucket(Bucket='bucket2')
@@ -16,11 +32,15 @@ def test_list_buckets():
 
 @mock_aws
 def test_list_buckets_no_buckets():
+    '''Test the list_buckets function when there are no buckets in the account.
+    This function should return an empty list.'''
     buckets = list_buckets()
     assert buckets == []
 
 @mock_aws
 def test_list_buckets_many_buckets():
+    '''Test the list_buckets function when there are many buckets in the account.
+    This function should return a list of all the buckets in the account.'''
     conn = boto3.resource('s3', region_name='us-east-1')
     for i in range(1000):
         conn.create_bucket(Bucket=f'bucket{i}')
@@ -30,6 +50,8 @@ def test_list_buckets_many_buckets():
 
 @mock_aws
 def test_select_bucket(monkeypatch):
+    '''Test the select_bucket function.
+    This function should prompt the user to select a bucket and return the selected bucket.'''
     conn = boto3.resource('s3', region_name='us-east-1')
     conn.create_bucket(Bucket='test_bucket')
     monkeypatch.setattr('builtins.input', lambda _: 'test_bucket')
@@ -37,13 +59,15 @@ def test_select_bucket(monkeypatch):
 
 @mock_aws
 def test_backup_files():
+    '''Test the backup_files function.
+    This function should backup the files in the specified folder to the specified bucket.'''
     # Check if the test directory exists
     if os.path.exists('test_folder'):
         # If it exists, remove it and all its contents
         shutil.rmtree('test_folder')
     os.mkdir('test_folder')
-    with open('test_folder/test_file.txt', 'w') as f:
-        f.write('test content')
+    with open('test_folder/test_file.txt', 'w') as test_file:
+        test_file.write('test content')
 
     conn = boto3.resource('s3', region_name='us-east-1')
     conn.create_bucket(Bucket='test_bucket')
@@ -56,6 +80,8 @@ def test_backup_files():
 
 @mock_aws
 def test_backup_files_no_folder():
+    '''Test the backup_files function when the specified folder does not exist.
+    This function should print an error message and return.'''
     conn = boto3.resource('s3', region_name='us-east-1')
     conn.create_bucket(Bucket='test_bucket')
     backup_files('test_bucket', 'test_folder')
@@ -63,13 +89,15 @@ def test_backup_files_no_folder():
 
 @mock_aws
 def test_backup_files_bucket_nonexistent():
+    '''Test the backup_files function when the specified bucket does not exist.
+    This function should print an error message and return.'''
     # Check if the test directory exists
     if os.path.exists('test_folder'):
         # If it exists, remove it and all its contents
         shutil.rmtree('test_folder')
     os.mkdir('test_folder')
-    with open('test_folder/test_file.txt', 'w') as f:
-        f.write('test content')
+    with open('test_folder/test_file.txt', 'w') as test_file:
+        test_file.write('test content')
     conn = boto3.resource('s3', region_name='us-east-1')
     backup_files('test_bucket', 'test_folder')
     with pytest.raises(Exception):
@@ -79,6 +107,8 @@ def test_backup_files_bucket_nonexistent():
 
 @mock_aws
 def test_list_objects():
+    '''Test the list_objects function.
+    This function should return a list of all the objects in the specified bucket.'''
     conn = boto3.resource('s3', region_name='us-east-1')
     bucket = conn.create_bucket(Bucket='test_bucket')
     bucket.put_object(Key='object1', Body=b'content1')
@@ -89,6 +119,8 @@ def test_list_objects():
 
 @mock_aws
 def test_download_object():
+    '''Test the download_object function.
+    This function should download the specified object from the specified bucket.'''
     if os.path.exists('test_object'):
         os.remove('test_object')
     conn = boto3.resource('s3', region_name='us-east-1')
@@ -102,6 +134,8 @@ def test_download_object():
 
 @mock_aws
 def test_generate_presigned_url():
+    '''Test the generate_presigned_url function.
+    This function should generate a presigned URL for the specified object'''
     conn = boto3.resource('s3', region_name='us-east-1')
     bucket = conn.create_bucket(Bucket='test_bucket')
     bucket.put_object(Key='test_object', Body=b'content')
@@ -111,6 +145,8 @@ def test_generate_presigned_url():
 
 @mock_aws
 def test_delete_object():
+    '''Test the delete_object function.
+    This function should delete the specified object from the specified bucket.'''
     conn = boto3.resource('s3', region_name='us-east-1')
     bucket = conn.create_bucket(Bucket='test_bucket')
     bucket.put_object(Key='test_object', Body=b'content')
@@ -120,28 +156,63 @@ def test_delete_object():
 
 @mock_aws
 def test_upload():
-     # Check if the test directory exists
+    '''Test the upload function.
+    This function should upload the files in the specified folder to the specified bucket.'''
+    # Check if the test directory exists
     if os.path.exists('test_folder'):
         # If it exists, remove it and all its contents
         shutil.rmtree('test_folder')
     os.mkdir('test_folder')
     os.mkdir('test_folder/subfolder')  # Create a subfolder
-    with open('test_folder/subfolder/test_file.txt', 'w') as f:
-        f.write('test content')
+    with open('test_folder/subfolder/test_file.txt', 'w') as test_file:
+        test_file.write('test content')
 
     conn = boto3.resource('s3', region_name='us-east-1')
     conn.create_bucket(Bucket='test_bucket')
 
     upload('test_bucket', 'test_folder')
     assert len(list(conn.Bucket('test_bucket').objects.all())) == 1
-    assert list(conn.Bucket('test_bucket').objects.all())[0].key == '/subfolder/test_file.txt'  # Check the key includes the subfolder
+    # Check the key includes the subfolder
+    assert list(conn.Bucket('test_bucket').objects.all())[0].key == '/subfolder/test_file.txt'
 
-    os.remove('test_folder/subfolder/test_file.txt')
-    os.rmdir('test_folder/subfolder')  # Remove the subfolder
-    os.rmdir('test_folder')
+    shutil.rmtree('test_folder')
+
+@mock_aws
+def test_upload_no_folder():
+    '''Test the upload function when the specified folder does not exist.
+    This function should return without uploading anything.'''
+    conn = boto3.resource('s3', region_name='us-east-1')
+    conn.create_bucket(Bucket='test_bucket')
+    upload('test_bucket', 'test_folder')
+    assert len(list(conn.Bucket('test_bucket').objects.all())) == 0
+
+@mock_aws
+def test_upload_large_file():
+    '''Test the upload function when the specified folder contains a large file.
+    This function should split the file into smaller chunks
+    and upload them to the specified bucket.'''
+    # Check if the test directory exists
+    if os.path.exists('test_folder'):
+        # If it exists, remove it and all its contents
+        shutil.rmtree('test_folder')
+    os.mkdir('test_folder')
+    os.mkdir('test_folder/subfolder')  # Create a subfolder
+    #create large file 5GB
+    with open('test_folder/subfolder/large_file.txt', 'wb') as test_file:
+        test_file.write(os.urandom(5 * 1024**3))
+
+    conn = boto3.resource('s3', region_name='us-east-1')
+    conn.create_bucket(Bucket='test_bucket')
+
+    upload('test_bucket', 'test_folder')
+    assert len(list(conn.Bucket('test_bucket').objects.all())) == 1
+
+    shutil.rmtree('test_folder')
 
 @mock_aws
 def test_list_contents():
+    '''Test the list_contents function.
+    This function should return a list of all the objects in the specified folder.'''
     conn = boto3.resource('s3', region_name='us-east-1')
     bucket = conn.create_bucket(Bucket='test_bucket')
     folder_name = 'test_folder'
@@ -158,6 +229,8 @@ def test_list_contents():
 
 @mock_aws
 def test_get_file():
+    '''Test the get_file function.
+    This function should download the specified object from the specified bucket.'''
     if os.path.exists('test_file.txt'):
         os.remove('test_file.txt')
 
@@ -171,3 +244,10 @@ def test_get_file():
     assert os.path.exists('test_file.txt')
 
     os.remove('test_file.txt')
+
+@mock_aws
+def test_get_file_nonexistent():
+    '''Test the get_file function when the specified object does not exist.
+    This function should print an error message and return.'''
+    get_file('test_bucket', 'test_folder', 'test_file.txt')
+    assert not os.path.exists('test_file.txt')
