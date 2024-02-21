b7bd213c0f260f11896672236aebe6a74dad4207
JZhang2024 <jackie.zhang@uconn.edu>
2024-02-21 18:23:05 -0500

    Implemented retrieval of file metadata and dynamo put operation

--- hw05/lambda_function.py
+++ hw05/lambda_function.py
@@ -5,6 +5,7 @@ import boto3
 print('Loading function')
 
 s3 = boto3.client('s3')
+dynamo = boto3.client('dynamodb')
 
 
 def lambda_handler(event, context):
@@ -15,7 +16,10 @@ def lambda_handler(event, context):
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
-        print("CONTENT TYPE: " + response['ContentType'])
+        #retrieve metadata from object
+        metadata = response['Metadata']
+        #write metadata to dynamodb table with primary key as file_name
+        dynamo.put_item(TableName='hw05-table', Item={'file_name':{'S':key},'metadata':{'M':metadata}})
         return response['ContentType']
     except Exception as e:
         print(e)
