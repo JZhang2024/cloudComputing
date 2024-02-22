7733b75c2bf36269960258d4ff5a53e11ddc0bd5
JZhang2024 <jackie.zhang@uconn.edu>
2024-02-21 19:44:11 -0500

    Fixed retrieval of specific metadata fields and subsequent dynamo put

--- hw05/lambda_function.py
+++ hw05/lambda_function.py
@@ -16,13 +16,29 @@ def lambda_handler(event, context):
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
-        #retrieve metadata from object
-        metadata = response['Metadata']
+        #retrieve file size from s3 object
+        size = response['ContentLength']
+        size_with_unit = str(size) + ' bytes'  # Specify the unit of file size
+        
+        #retrieve upload date from s3 object
+        last_modified = response['LastModified']
+        #retrieve file ARN from s3 object
+        arn = response['ResponseMetadata']['HTTPHeaders']['x-amz-request-id']
+        #retrieve entity tag from s3 object
+        etag = response['ETag']
+        
         #write metadata to dynamodb table with primary key as file_name
-        dynamo.put_item(TableName='hw05-table', Item={'file_name':{'S':key},'metadata':{'M':metadata}})
-        return response['ContentType']
+        item = {
+            'file_name': {'S': key},
+            'size': {'S': size_with_unit},  # Store the size with unit
+            'last_modified': {'S': str(last_modified)},
+            'arn': {'S': arn},
+            'etag': {'S': etag}
+        }
+        
+        dynamo.put_item(TableName='hw05-table', Item=item)
+        #return response['ContentType']
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
-              
