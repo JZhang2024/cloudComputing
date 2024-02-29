f6b7702cfeb3a5cd7740b5c39ff0e43c74440e04
JZhang2024 <jackie.zhang@uconn.edu>
2024-02-28 21:48:16 -0500

    implemented lambda for listing buckets in account

---
+++ hw06/list_buckets_handler.py
@@ -0,0 +1,19 @@
+'''lambda function to list all the buckets in the AWS account'''
+
+import json
+import boto3
+
+def lambda_handler(event, context):
+    '''lambda function to list all the buckets in the AWS account'''
+    s3 = boto3.client('s3')
+    response = s3.list_buckets()
+    #output for cloudwatch log
+    if response.get('Buckets') is not None:
+        for bucket in response['Buckets']:
+            print(f'{bucket["Name"]}')
+    else:
+        print('No buckets found')
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
