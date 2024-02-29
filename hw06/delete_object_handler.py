8cdb843bb29f845069959834ef19b7ee0bfe8735
JZhang2024 <jackie.zhang@uconn.edu>
2024-02-28 23:32:21 -0500

    implemented delete object lambda

---
+++ hw06/delete_object_handler.py
@@ -0,0 +1,33 @@
+'''lambda function to delete an object from a bucket'''
+
+import json
+import boto3
+
+def delete_object_lambda_handler(event, context):
+    '''Delete object from the given bucketname. The object name
+    is passed in the "body" of the post request'''
+
+    # Get the bucketname and the object name from the request
+    body = json.loads(event['body'])
+    bucketname = body['bucketname']
+    objectname = body['objectname']
+
+    print(f'Deleting object {objectname} from bucket {bucketname}')
+
+    # Create a new s3 client
+    s3_client = boto3.client('s3')
+
+    # Delete the object from the bucket
+    s3_client.delete_object(Bucket=bucketname, Key=objectname)
+
+    print(f'Successfully deleted object {objectname} from bucket {bucketname}')
+
+    # Return the response
+    response = {
+        'bucketname': bucketname,
+        'objectname': objectname
+    }
+    return {
+        'statusCode': 200,
+        'body': json.dumps(response)
+    }
