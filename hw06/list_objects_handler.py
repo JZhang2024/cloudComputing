5bb5b884f8bbc887b76cd16dbfc78aa2bacb15d7
JZhang2024 <jackie.zhang@uconn.edu>
2024-02-28 22:04:41 -0500

    Added log output for bucket name parameter

--- hw06/list_objects_handler.py
+++ hw06/list_objects_handler.py
@@ -6,8 +6,8 @@ import boto3
 def list_objects_lambda_handler(event, context):
     '''lambda function for listing all the objects in a bucket'''
     s3 = boto3.client('s3')
+    print(f'Listing objects in bucket: {event["bucket"]}')
     response = s3.list_objects(Bucket=event['bucket'])
-    #output for cloudwatch log
     if response.get('Contents') is not None:
         for obj in response['Contents']:
             print(f'{obj["Key"]}')
