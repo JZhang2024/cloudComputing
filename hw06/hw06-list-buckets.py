0d01310979f23ea36b76825c2488489b0f3258e2
JZhang2024 <jackie.zhang@uconn.edu>
2024-03-01 18:14:35 -0500

    updated lambda logic for request model

--- hw06/hw06-list-buckets.py
+++ hw06/hw06-list-buckets.py
@@ -1,23 +1,28 @@
-'''lambda function to list all the buckets in the AWS account'''
-
 import json
 import boto3
+
+'''lambda function to list all the buckets in the AWS account'''
+
 
 def list_buckets_lambda_handler(event, context):
     '''lambda function to list all the buckets in the AWS account'''
     s3_client = boto3.client('s3')
-    response = s3_client.list_buckets()
+    buckets = s3_client.list_buckets()
     #output for cloudwatch log
     sts = boto3.client('sts')
     account_id = sts.get_caller_identity()['Account']
     print(f'Listing all buckets in the AWS account {account_id}')
-    if response.get('Buckets') is not None:
-        for bucket in response['Buckets']:
+    bucket_names = []
+    if buckets.get('Buckets') is not None:
+        for bucket in buckets['Buckets']:
+            bucket_names.append(bucket["Name"])
             print(f'{bucket["Name"]}')
     else:
         print('No buckets found')
-    return {
+
+    response = {
         'statusCode': 200,
         'headers': { 'Content-Type': 'application/json' },
-        'body': json.dumps(response)
+        'body': json.dumps({'buckets': bucket_names})
     }
+    return response
--- hw06/hw06-list-objects.py
+++ hw06/hw06-list-objects.py
@@ -1,20 +1,27 @@
-'''lambda function for listing all the objects in a bucket'''
-
 import json
 import boto3
+
+'''lambda function for listing all the objects in a bucket'''
 
 def list_objects_lambda_handler(event, context):
     '''lambda function for listing all the objects in a bucket'''
     s3_client = boto3.client('s3')
-    print(f'Listing objects in bucket: {event["bucket"]}')
-    response = s3_client.list_objects(Bucket=event['bucket'])
+
+    # Get the bucketname from the request
+    body = json.loads(event['body'])
+    bucketname = body['bucketname']
+
+    print(f'Listing objects in bucket: {bucketname}')
+    response = s3_client.list_objects(Bucket=bucketname)
+    object_names = []
     if response.get('Contents') is not None:
         for obj in response['Contents']:
+            object_names.append(obj["Key"])
             print(f'{obj["Key"]}')
     else:
         print('No objects found')
     return {
         'statusCode': 200,
         'headers': { 'Content-Type': 'application/json' },
-        'body': json.dumps(response)
+        'body': json.dumps({'objects': object_names})
     }
