9f795e9b96e8414fb8c909d1b1ec8f2ac394050f
JZhang2024 <jackie.zhang@uconn.edu>
2024-03-01 21:05:41 -0500

    fixed incorrect key for objectname/file_name

--- hw06/hw06-add-object.py
+++ hw06/hw06-add-object.py
@@ -9,9 +9,9 @@ def add_object_lambda_handler(event, context):
     and the file contents are passed in the "body" of the post request'''
 
     # Get the bucketname and the object name from the request
-    print(event)
+    print(event) #log the event
     bucketname = event['params']['path']['bucket-name']
-    objectname = event['body-json']['objectname']
+    objectname = event['body-json']['file_name']
     objectcontent = base64.b64decode(event['body-json']['body'])
 
     print(f'Adding object {objectname} to bucket {bucketname}')
