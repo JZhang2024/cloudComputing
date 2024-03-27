92dd2c85d8a300f7b6a0c2de975ee1aaec4634d8
JZhang2024 <jackie.zhang@uconn.edu>
2024-03-26 21:25:56 -0400

    Fixed edge case of KeyError on retrieval of objects when no objects exist yet

--- hw07/hw3/util.py
+++ hw07/hw3/util.py
@@ -3,7 +3,9 @@
 def get_s3_file_list(bucket_name, s3, file_extension):
     '''Get a list of all files in an S3 bucket with a specific extension'''
     files = []
-    for obj in s3.list_objects(Bucket=bucket_name)['Contents']:
-        if obj['Key'].endswith(file_extension):
-            files.append(obj['Key'])
+    response = s3.list_objects(Bucket=bucket_name)
+    if 'Contents' in response:
+        for obj in response['Contents']:
+            if obj['Key'].endswith(file_extension):
+                files.append(obj['Key'])
     return files
